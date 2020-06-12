6e9964ad13642eec405b312b67e96aa8b5de8f09
milkiasghebremariam <milkiastgt@gmail.com>
2020-06-12 16:18:53 -0700

    complate step 2

--- src/main/java/com/mtg/bookstore/BookstoreApplication.java
+++ src/main/java/com/mtg/bookstore/BookstoreApplication.java
@@ -1,13 +1,24 @@
 package com.mtg.bookstore;
 
+import com.mtg.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
+
+
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(BookstoreApplication.class, args);
+
+
+
+
     }
 
 }
